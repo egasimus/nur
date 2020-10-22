2e984b0653fefe325083a3e3b69934958638facb
Adam Avramov <my.new@email.here>
2020-10-23 00:58:02 +0200

    shorten hash in wineasio and move pkgconfig to nativeBuildInputs

--- pkgs/wineasio/default.nix
+++ pkgs/wineasio/default.nix
@@ -1,18 +1,23 @@
-{pkgs,stdenv,fetchgit,...}: let
+{stdenv
+,fetchgit
+,pkgconfig
+,pkgs
+,...}: let
+  repo = "wineasio";
+  rev  = "e7174186";
   jack = pkgs.jack2;
   wine = pkgs.wineWowPackages.staging;
 in stdenv.mkDerivation rec {
-  pname = "wineasio";
-  rev = "e71741863fc89ac5c78eba2018ec1737499a287b";
-  name = "${pname}-${rev}";
+  name = "${repo}-${rev}";
   src = fetchgit {
-    url = "https://github.com/wineasio/wineasio.git";
+    url = "https://github.com/wineasio/${repo}.git";
     sha256 = "0an96bp683n7wwwwa2f6945v2pn4q2q6vn0v73kc0k5cqns11qc6";
     fetchSubmodules = true;
     inherit rev;
   };
   patches = [ ./prefix.patch ];
-  buildInputs = [ jack wine pkgs.pkgconfig ];
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ jack wine ];
   buildPhase = ''
     make PREFIX=${wine.out} 64
   '';
