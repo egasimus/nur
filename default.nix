60780d6f250863ba15ae22f555d1b05530dc4565
Adam Avramov <adam.b.avramov@gmail.com>
2021-01-20 22:24:55 +0100

    organize packages in categories

--- default.nix
+++ default.nix
@@ -7,27 +7,38 @@ in {
   modules  = import ./__modules;               # NixOS modules
   overlays = import ./__overlays;              # nixpkgs overlays
 
-  # programs
-  _100r                = pkg ./_100r;
-  arcan                = pkg ./arcan;
-  butterflow           = pkg ./butterflow;
-  caprine              = pkg ./caprine;
   chkservice           = pkg ./chkservice;
+
+  # clients
+  caprine              = pkg ./caprine;
+  gh                   = pkg ./gh;
+
+  # gui
+  arcan                = pkg ./arcan;
+  gxkb                 = pkg ./gxkb;
+  pugmenu              = pkg ./pugmenu;
+
+  # network
+  orjail               = pkg ./orjail;
+  tor                  = pkg ./tor;
+
+  # art
+  _100r                = pkg ./_100r;
+  hydra                = pkg ./hydra;
+  ossia                = pkg ./ossia;
+  praxis-live          = pkg ./praxis-live;
+  troop                = pkg ./troop;
+
+  # video
+  butterflow           = pkg ./butterflow;
   cinelerra-gg         = pkg ./cinelerra-gg;
   #davinci-resolve      = pkg ./davinci-resolve;
+
+  # audio
   din                  = pkg ./din;
-  gh                   = pkg ./gh;
-  gxkb                 = pkg ./gxkb;
-  hydra                = pkg ./hydra;
   jack_midi_clock      = pkg ./jack_midi_clock;
-  orjail               = pkg ./orjail;
-  ossia                = pkg ./ossia;
   pavucontrol-vertical = pkg ./pavucontrol-vertical;
-  praxis-live          = pkg ./praxis-live;
-  pugmenu              = pkg ./pugmenu;
   reaper               = pkg ./reaper;
   scarlett-mixer       = pkg ./scarlett-mixer;
-  tor                  = pkg ./tor;
-  troop                = pkg ./troop;
   wineasio             = pkg ./wineasio;
 }
