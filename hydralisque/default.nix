28417b0943c01b919f81ff3a7a4b8f0a5676c0dd
Adam Avramov <adam.b.avramov@gmail.com>
2021-01-29 20:40:57 +0100

    fix hydralisque and use mkNodePackage in pugmenu

--- hydralisque/default.nix
+++ hydralisque/default.nix
@@ -9,29 +9,36 @@
 , yarn
 }: let
 
+  rev    = "e730d7af04c28cff628e7074d49226d22e363e19";
+  sha256 = "0cgz9556y21da2dmf3zcr30p71ba3pkxbrlbbnhl89f44zswg000";
   mkNodePackage = (import ../mk-node-package.nix) { inherit lib runCommand; };
 
 in mkNodePackage {
-  name       = "hydralisque";
+  name = "hydralisque-${rev}";
   outputHash = "0rpm6jvamx7rycarw78y17gzrkkkrs4sydin8lylx4435sa1p4s1";
   meta = {
     description = "video masher based on Olivia Jack's Hydra";
     homepage = "https://github.com/egasimus/hydralisque";
     license = lib.licenses.agpl3Only;
   };
+  src = fetchFromGitHub {
+    owner = "egasimus";
+    repo  = "hydralisque";
+    inherit rev sha256;
+  };
   entryPoint = ''
     #!/bin/sh
     pushd $(dirname $(dirname $0))
-    ${electron_11.out}/bin/electron electron/app.js
+    ${electron_11.out}/bin/electron electron/app.js $@
   '';
   buildInputs = [
-    nodejs-14_x
     electron_11
-    pkg-config
-    yarn
     electron_11.buildInputs
+    nodejs-14_x
+    pkg-config
     xlibs.libX11
-    xlibs.libXi
     xlibs.libXext
+    xlibs.libXi
+    yarn
   ];
 }
--- pugmenu/default.nix
+++ pugmenu/default.nix
@@ -1,39 +1,60 @@
-{cairo
-,dbus
-,fetchFromGitHub
-,glib
-,gobject-introspection
-,gtk3
-,lib
-,mkYarnPackage
-,nodePackages
-,nodejs-10_x
-,pkg-config
-,python2
-,...}: let
-  rev    = "9b98f5f";
-  sha256 = "0cgz9556y21da2dmf3zcr30p71ba3pkxbrlbbnhl89f44zswg5ry";
-in mkYarnPackage {
+{ lib
+, cairo
+, dbus
+, fetchFromGitHub
+, glib
+, gobject-introspection
+, gtk3
+, nodejs-14_x
+, pkg-config
+, python2
+, runCommand
+, yarn
+}: let
 
+  node       = nodejs-14_x;
+  rev        = "9b98f5f";
+  sha256     = "0cgz9556y21da2dmf3zcr30p71ba3pkxbrlbbnhl89f44zswg5ry";
+  outputHash = "1zk1wm13sbs4jpr9g47yy7aq4vrm198b1psnbw750jlclhj5w2dw";
+  mkNodePackage = (import ../mk-node-package.nix) { inherit lib runCommand; };
+
+in mkNodePackage {
+
+  name = "pugmenu";
   meta = {
-    broken = true;
+    description = "tray icon, configurable with Pug";
+    homepage = "https://github.com/egasimus/hydralisque";
+    license = lib.licenses.gpl3Only;
   };
-
-  name = "pugmenu-${rev}";
   src = fetchFromGitHub {
     owner = "egasimus";
     repo  = "pugmenu";
     inherit rev sha256;
   };
-  nativeBuildInputs = [python2 pkg-config nodePackages.npm];
-  buildInputs = [gtk3 gobject-introspection glib cairo dbus];
-  propagatedBuildInputs = [nodejs-10_x];
-  LD_LIBRARY_PATH = lib.strings.makeLibraryPath [
-    gtk3
-    gobject-introspection
-    glib
+  inherit outputHash;
+  entryPoint = ''
+    #!/bin/sh
+    pushd $(dirname $(dirname $0))
+    ${node.out}/bin/node pugmenu $@
+  '';
+
+  buildInputs = [
     cairo
     dbus
+    glib
+    gobject-introspection
+    gtk3
+    node
+    pkg-config
+    python2
+    yarn
   ];
-  preConfigure = "ls -al";
+  #LD_LIBRARY_PATH = lib.strings.makeLibraryPath [
+    #gtk3
+    #gobject-introspection
+    #glib
+    #cairo
+    #dbus
+  #];
+  #preConfigure = "ls -al";
 }
